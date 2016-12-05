15f49465086dcb592d310ce2167b2f7f778c5936
wanganqi123 <569450181@qq.com>
2016-12-05 12:42:29 +0800

    前台（修改1）适配于动态代码

---
+++ Desktop/Application/Home/Controller/YaoquController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class YaoquController extends Controller {
+    public function index(){
+       
+         $this->display();
+     }
+}
---
+++ Desktop/Controller/LiuxueController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class LiuxueController extends Controller {
+    public function index(){
+       
+         $this->display();
+     }
+}
